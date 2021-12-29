7ca1769a4f900907de0c15ed17ddef53414469a8
tomonakar <tomohisa.nak@gmail.com>
2021-12-29 18:25:00 +0900

    feat: シンプルなjsonを返すAPIを作成

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -10,5 +10,6 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
 	return router
 }
--- backend/models/models.go
+++ backend/models/models.go
@@ -6,8 +6,10 @@ type Movie struct {
 	ID          int          `json:"id"`
 	Title       string       `json:"title"`
 	Description string       `json:"description"`
-	ReleaseDate string       `json:"release_date"`
+	Year        string       `json:"year"`
+	ReleaseDate time.Time    `json:"release_date"`
 	Runtime     int          `json:"runtime"`
+	Rating      int          `json:"rating"`
 	MPAARating  string       `json:"mpaa_rating"`
 	CreatedAt   time.Time    `json:"created_at"`
 	UpdatedAt   time.Time    `json:"updated_at"`
