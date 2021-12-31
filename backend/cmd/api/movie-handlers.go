c239ef96d59c079a1f82a8915c1176ba956a4dd0
tomonakar <tomohisa.nak@gmail.com>
2021-12-31 13:20:58 +0900

    feat: 全てのジャンルを取得するAPIを作成

--- backend/cmd/api/movie-handlers.go
+++ backend/cmd/api/movie-handlers.go
@@ -45,6 +45,19 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
+	genres, err := app.models.DB.GenresAll()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, genres, "genres")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
 func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {}
 func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {}
 func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {}
--- backend/models/movies-db.go
+++ backend/models/movies-db.go
@@ -141,3 +141,34 @@ func (m *DBModel) All() ([]*Movie, error) {
 	}
 	return movies, nil
 }
+
+func (m *DBModel) GenresAll() ([]*Genre, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `select id, genre_name, created_at, updated_at from genres order by genre_name
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var genres []*Genre
+
+	for rows.Next() {
+		var genre Genre
+		err := rows.Scan(
+			&genre.ID,
+			&genre.GenreName,
+			&genre.CreatedAt,
+			&genre.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		genres = append(genres, &genre)
+	}
+	return genres, nil
+}
