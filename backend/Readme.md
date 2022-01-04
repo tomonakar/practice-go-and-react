fb3204d8563b26373cecd8012f51ec8ca3556814
tomonakar <tomohisa.nak@gmail.com>
2022-01-04 13:34:52 +0900

    chore: jwtとcrypotパッケージを追加

--- backend/Readme.md
+++ backend/Readme.md
@@ -1,3 +1,4 @@
 - [httprouter](https://github.com/julienschmidt/httprouter)
 - [pq](https://github.com/lib/pq)
+- [jwt](https://github.com/pascaldekloe/jwt)
 
---
+++ backend/cmd/api/tokens.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"backend/models"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/pascaldekloe/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var validUser = models.User{
+	ID:       10,
+	Email:    "ttt@gmail.com",
+	Password: "$2a$12$krcITqLRoyYGHv/OvgCiEuuriC39023lCqL1mqMRgyX50D0hz9.GW",
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (app *application) Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthenticated"))
+		return
+	}
+
+	hashedPassword := validUser.Password
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthenticated"))
+		return
+	}
+
+	var claims jwt.Claims
+	claims.Subject = fmt.Sprint(validUser.ID)
+	claims.Issued = jwt.NewNumericTime(time.Now())
+	claims.NotBefore = jwt.NewNumericTime(time.Now())
+	claims.Expires = jwt.NewNumericTime(time.Now().Add(time.Hour * 24))
+	claims.Issuer = "mydomain.com"
+	claims.Audiences = []string{"mydomain.com"}
+
+	jwtBytes, err := claims.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
+	if err != nil {
+		app.errorJSON(w, errors.New("error signing"))
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, jwtBytes, "response")
+
+}
