b02ff7ec2f83e60f37a05a6c8235a31452663cb0
tomonakar <tomohisa.nak@gmail.com>
2022-01-04 13:44:58 +0900

    feat: ログイン処理を実装

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -8,55 +8,56 @@ export default class Admin extends Component {
     error: null,
   }
 
-  async componentDidMount() {
-    try {
-      const resp = await fetch("http://localhost:4000/v1/movies")
-
-      if (resp.status !== 200) {
-        this.setState({
-          error: `Invalid response code: ${resp.status}`,
-          isLoaded: true,
-        })
-        return
-      }
-
-      const data = await resp.json()
-      console.log({ data })
-      this.setState({ movies: data.movies, isLoaded: true })
-    } catch (error) {
-      console.log({ error })
-      this.setState({ error: error.message, isLoaded: true })
-    }
+  componentDidMount() {
+    fetch("http://localhost:4000/v1/movies")
+      .then((response) => {
+        if (response.status !== "200") {
+          let err = Error
+          err.message = "Invalid response code: " + response.status
+          this.setState({ error: err })
+        }
+        return response.json()
+      })
+      .then((json) => {
+        this.setState(
+          {
+            movies: json.movies,
+            isLoaded: true,
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            })
+          },
+        )
+      })
   }
 
   render() {
     const { movies, isLoaded, error } = this.state
-
-    console.log({ movies })
-
     if (error) {
-      return <p>{error}</p>
-    }
-
-    if (!isLoaded) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
       return <p>Loading...</p>
+    } else {
+      return (
+        <Fragment>
+          <h2>Manage Catalogue</h2>
+          <hr />
+          <div className="list-group">
+            {movies.map((m) => (
+              <Link
+                key={m.id}
+                className="list-group-item list-group-item-action"
+                to={`/admin/movie/${m.id}`}
+              >
+                {m.title}
+              </Link>
+            ))}
+          </div>
+        </Fragment>
+      )
     }
-    return (
-      <Fragment>
-        <h2>Manage Catalogue</h2>
-
-        <div className="list-group">
-          {movies.map((m) => (
-            <Link
-              key={m.id}
-              className="list-group-item list-group-item-action"
-              to={`/admin/movie/${m.id}`}
-            >
-              {m.title}
-            </Link>
-          ))}
-        </div>
-      </Fragment>
-    )
   }
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,10 @@
-import React, { Component, Fragment } from "react";
-import Input from "./form-components/Input";
-import Alert from "./ui-components/Alert";
+import React, { Component, Fragment } from "react"
+import Input from "./form-components/Input"
+import Alert from "./ui-components/Alert"
 
 export default class Login extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       email: "",
@@ -15,27 +15,66 @@ export default class Login extends Component {
         type: "d-none",
         message: "",
       },
-    };
+    }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange = (evt) => {
-    let value = this.target.value;
-    let name = this.target.name;
+    let value = evt.target.value
+    let name = evt.target.name
     this.setState((prevState) => ({
       ...prevState,
       [name]: value,
-    }));
-  };
+    }))
+  }
 
   handleSubmit = (evt) => {
-    evt.preventDefault();
-  };
+    evt.preventDefault()
+
+    let errors = []
+
+    if (this.state.email === "") {
+      errors.push("email")
+    }
+
+    if (this.state.password === "") {
+      errors.push("password")
+    }
+
+    this.setState({ errors: errors })
+
+    if (errors.length > 0) {
+      return false
+    }
+
+    const data = new FormData(evt.target)
+    const payload = Object.fromEntries(data.entries())
+
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(payload),
+    }
+
+    fetch("http://localhost:4000/v1/signin", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          this.setState({
+            alert: {
+              type: "alert-danger",
+              message: data.error.message,
+            },
+          })
+        } else {
+          console.log(data)
+        }
+      })
+  }
 
   hasError(key) {
-    return this.state.errors.indexOf(key) !== -1;
+    return this.state.errors.indexOf(key) !== -1
   }
 
   render() {
@@ -73,6 +112,6 @@ export default class Login extends Component {
           <button className="btn btn-primary">Login</button>
         </form>
       </Fragment>
-    );
+    )
   }
 }
