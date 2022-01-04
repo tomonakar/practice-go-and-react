01b3ab13045c78e40175597b798fb1d4c9cefe93
tomonakar <tomohisa.nak@gmail.com>
2022-01-04 16:27:09 +0900

    feat: 有効なトークンであることをチェックするミドルウェアを追加

--- backend/Readme.md
+++ backend/Readme.md
@@ -1,6 +1,7 @@
 - [httprouter](https://github.com/julienschmidt/httprouter)
 - [pq](https://github.com/lib/pq)
 - [jwt](https://github.com/pascaldekloe/jwt)
+- [Alice](https://github.com/justinas/alice)
 
 - [ダミーのsecret作成](https://go.dev/play/p/s8KlqJIOWej)
 - [ダミー暗号化済パスワード作成](https://go.dev/play/p/uKMMCzJWGsW)
--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -1,11 +1,73 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pascaldekloe/jwt"
+)
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		next.ServeHTTP(w, r)
+	})
 
+}
+
+func (app *application) checkToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Very", "authorization")
+
+		authHeader := r.Header.Get("Authorization")
+
+		if authHeader == "" {
+			// could set an anonymous user here
+		}
+
+		headerParts := strings.Split(authHeader, " ")
+		if len(headerParts) != 2 {
+			app.errorJSON(w, errors.New("Invalid authorization header"))
+		}
+
+		if headerParts[0] != "Bearer" {
+			app.errorJSON(w, errors.New("unauthorized - no bearer"))
+		}
+
+		token := headerParts[1]
+
+		claims, err := jwt.HMACCheck([]byte(token), []byte(app.config.jwt.secret))
+		if err != nil {
+			app.errorJSON(w, errors.New("unauthorized - failed hmac check"))
+		}
+
+		if claims.Valid(time.Now()) {
+			app.errorJSON(w, errors.New("unauthorized - token expired"))
+			return
+		}
+
+		if !claims.AcceptAudience("mydomain.com") {
+			app.errorJSON(w, errors.New("unauthorized - invalid audience"))
+			return
+		}
+
+		if claims.Issuer != "mydomain.com" {
+			app.errorJSON(w, errors.New("unauthorized - invalid issuer"))
+			return
+		}
+
+		userID, err := strconv.ParseInt(claims.Subject, 10, 64)
+		if err != nil {
+			app.errorJSON(w, errors.New("unauthorized"))
+			return
+		}
+
+		log.Println("userID:", userID)
 		next.ServeHTTP(w, r)
 	})
 
