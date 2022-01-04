2a0de50e12217f818c1d6914701d737b08042f1f
tomonakar <tomohisa.nak@gmail.com>
2022-01-04 11:39:46 +0900

    feat: 編集ページへの導線追加

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,10 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react"
+import { Link } from "react-router-dom"
 
 export default class Admin extends Component {
-    
-    render() {
-        return(
-            <h2>Manage Catalogue</h2>
-        );
+  state = {
+    movies: [],
+    isLoaded: false,
+    error: null,
+  }
+
+  async componentDidMount() {
+    try {
+      const resp = await fetch("http://localhost:4000/v1/movies")
+
+      if (resp.status !== 200) {
+        this.setState({
+          error: `Invalid response code: ${resp.status}`,
+          isLoaded: true,
+        })
+        return
+      }
+
+      const data = await resp.json()
+      console.log({ data })
+      this.setState({ movies: data.movies, isLoaded: true })
+    } catch (error) {
+      console.log({ error })
+      this.setState({ error: error.message, isLoaded: true })
     }
+  }
+
+  render() {
+    const { movies, isLoaded, error } = this.state
+
+    console.log({ movies })
+
+    if (error) {
+      return <p>{error}</p>
+    }
+
+    if (!isLoaded) {
+      return <p>Loading...</p>
+    }
+    return (
+      <Fragment>
+        <h2>Manage Catalogue</h2>
+
+        <div className="list-group">
+          {movies.map((m) => (
+            <Link
+              key={m.id}
+              className="list-group-item list-group-item-action"
+              to={`/admin/movie/${m.id}`}
+            >
+              {m.title}
+            </Link>
+          ))}
+        </div>
+      </Fragment>
+    )
+  }
 }
--- frontend/src/components/EditMovie.js
+++ frontend/src/components/EditMovie.js
@@ -73,8 +73,8 @@ export default class EditMovie extends Component {
             alert: { type: "alert-danger", message: data.error.message },
           })
         } else {
-          this.setState({
-            alert: { type: "alert-success", message: "Changes saved!" },
+          this.props.history.push({
+            pathname: "/admin",
           })
         }
       })
